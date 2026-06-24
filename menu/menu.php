e681decaef4db037a1ef76c84304027b3bb06ea3
Paulo Pereira <89421275+PauloPereira88@users.noreply.github.com>
2026-06-24 11:08:43 -0400

    menu

---
+++ menu/menu.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="#inicio">INICIO</a></li>
+            <li><a href="#contato">CONTATO</a></li>
+            <li><a href="#sobrenos">SOBRE-NOS</a></li>
+        </ul>
+    </nav>
+</header>
