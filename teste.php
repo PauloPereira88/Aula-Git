9a4ac3f403d0c260d26a9081e17f0e2fa80273db
Paulo Pereira <89421275+PauloPereira88@users.noreply.github.com>
2026-06-24 11:15:41 -0400

    separacaodoheader

--- teste.php
+++ teste.php
@@ -4,11 +4,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TESTE - GIT</title>
-</head>
+<?php
+    include './header/header;php';
+?>
 <body>
 
     <h1>TESTE DE GIT</h1>
